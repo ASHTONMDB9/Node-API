527f35b7fb2018373aaead673fb6abb591afadd4
ASHTONMDB9 <ashtonabrahams563@gmail.com>
2022-07-27 15:50:09 +0200

    by ID

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -16,7 +16,7 @@ router.get("/", (req, res) => {
 // Gets one users
 router.get("/:id", (req, res) => {
   try {
-    con.query("SELECT * FROM users WHERE user_id ${}", (err, result) => {
+    con.query(`SELECT * FROM users WHERE user_id = ${req.params.id}`, (err, result) => {
       if (err) throw err;
       res.send(result);
     });
